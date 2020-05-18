d69d03b477d61244a2b74e8c1c173f4324855939
Yan Magalhães <yanmagale@gmail.com>
2020-05-17 23:23:27 -0300

    chore: add linter styles with eslint and prettier to format files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # crypto_wallet
 A small application to give a user access to manager your cripto currencies
+
+
+- estlint airbnb
+- prettier
+- husky e commitlint
+- Stylelint ? Search
+
+
+- Create Account
+- Wallet Base
