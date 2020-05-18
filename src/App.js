7fe73c1886b16e59c7a750e16c8ba12fea91b2e1
Yan Magalhães <yanmagale@gmail.com>
2020-05-18 00:16:29 -0300

    chore: add support to styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
 function App() {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title>Works with styled components</Title>
       </header>
     </div>
   );
