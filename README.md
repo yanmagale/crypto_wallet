1608c786444068cc929e3d02a0b9d5910d5d2602
Yan Magalhães <yanmagale@gmail.com>
2020-05-17 23:28:05 -0300

    chore: add pattern to validate commits with commitlint

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # crypto_wallet
+
 A small application to give a user access to manager your cripto currencies
 
-
-- estlint airbnb
-- prettier
 - husky e commitlint
 - Stylelint ? Search
 
-
-- Create Account
-- Wallet Base
+* Create Account
+* Wallet Base
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -6,27 +7,27 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
+            loader: 'html-loader',
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
