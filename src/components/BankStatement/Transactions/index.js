5812ed1644e38f36bdb9de880636a8eb137f295a
Yan Magalhães <yanmagale@gmail.com>
2020-06-04 02:34:57 -0300

    refactor(bank-statement): send to Transactions component only data

--- src/components/BankStatement/Transactions/index.js
+++ src/components/BankStatement/Transactions/index.js
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
 
-import BankStatementService from 'services/bankStatement/';
-
 class BankStatementTransactions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transactions: [],
-    };
-  }
-
-  componentDidMount() {
-    BankStatementService.getEntries()
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          transactions: [...this.state.transactions, ...response],
-        });
-      })
-      .catch((error) => console.error(error));
-  }
-
   render() {
-    const { transactions } = this.state;
+    const { transactions } = this.props;
     return (
       <React.Fragment>
-        {transactions.length &&
-          transactions.map((transaction, index) => (
-            <div key={index}>
-              <p>{transaction.type}</p>
-              <p>{transaction.currency}</p>
-              <p>{transaction.amount}</p>
-              <p>{transaction.date}</p>
-            </div>
-          ))}
+        {transactions.map((transaction, index) => (
+          <div key={index}>
+            <p>{transaction.type}</p>
+            <p>{transaction.currency}</p>
+            <p>{transaction.amount}</p>
+            <p>{transaction.date}</p>
+          </div>
+        ))}
       </React.Fragment>
     );
   }
--- src/pages/BankStatement/index.js
+++ src/pages/BankStatement/index.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import BankStatementTransactions from 'components/BankStatement/Transactions';
+import BankStatementService from 'services/bankStatement/';
 
 class BankStatementPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [],
+    };
+  }
+
+  componentDidMount() {
+    BankStatementService.getEntries()
+      .then((response) => {
+        this.setState({
+          transactions: [...this.state.transactions, ...response],
+        });
+      })
+      .catch((error) => console.error(error));
+  }
   render() {
+    const { transactions } = this.state;
     return (
       <div>
         <h2>Bank Statement Page</h2>
-        <BankStatementTransactions />
+        {transactions.length && (
+          <BankStatementTransactions transactions={transactions} />
+        )}
+        <Link to={`/wallet`}>
+          <button>Acessar minha carteira</button>
+        </Link>
       </div>
     );
   }
