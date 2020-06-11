66d65a6d38cfe895f625d4509653cc425ec101de
Yan Magalhães <yanmagale@gmail.com>
2020-06-11 03:47:11 -0300

    style(sign-up): improve ui elements

--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import UsersService from 'services/users/';
 import SignUpForm from 'components/sign-up/form';
 
-import { Wrapper } from './style';
+import { Wrapper, Title, Container, ApplicationContent } from './style';
 
 class SignUpPage extends Component {
   constructor() {
@@ -29,15 +29,19 @@ class SignUpPage extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Crypto Wallet</h2>
+      <Container>
+        <Title>Crypto Wallet</Title>
         <div>
-          <p>Algum texto aqui falando sobre a aplicação</p>
+          <ApplicationContent>
+            Bem-vindo ao seu controle de crypto moedas. Crie sua conta e depois
+            gerencia as suas moedas virtuais.
+          </ApplicationContent>
         </div>
         <Wrapper>
+          <h2>Preencha os campos abaixo</h2>
           <SignUpForm handleCreateUser={this.createUser} />
         </Wrapper>
-      </div>
+      </Container>
     );
   }
 }
--- src/pages/sign-up/style.js
+++ src/pages/sign-up/style.js
@@ -1,12 +1,31 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 30vh;
-  width: 200px;
+  height: 50vh;
+  width: 80%;
   display: flex;
   align-items: center;
   justify-content: center;
-  padding-top: 10px;
   border-radius: 4px;
-  background: #cccccc;
+  background: #e9e9e9;
+  text-align: center;
+  margin: 30px auto;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  margin: 0;
+  padding-bottom: 5px;
+`;
+
+export const Container = styled.section`
+  padding: 5px 10px;
+`;
+
+export const ApplicationContent = styled.p`
+  margin: 0;
+  padding: 15px 10px;
+  font-size: 18px;
 `;
