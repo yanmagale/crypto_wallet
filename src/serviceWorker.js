c273670efa8664b94b7c5fa43fe77a0947b51d80
Yan Magalhães <yanmagale@gmail.com>
2020-06-11 19:48:03 -0300

    feat: use public url env

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -8,8 +8,13 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
     window.addEventListener('load', () => {
-      const swUrl = `./service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
