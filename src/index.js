8492c0887f3abcda1c43908257e3677562af9edd
Yan Magalhães <yanmagale@gmail.com>
2020-06-11 16:27:26 -0300

    refactor: render SignUp an Wallet Page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// import WalletPage from 'pages/wallet/';
 import SignUpPage from 'pages/sign-up/';
+import WalletPage from 'pages/wallet/';
 // import BankStatementPage from 'pages/bank-statement/';
 // import BuyCurrencyPage from 'pages/buy-currency/';
 import { Store } from 'redux-folder/store';
@@ -18,8 +18,8 @@ ReactDOM.render(
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={SignUpPage} />
-          {/* <Route path="/wallet" component={WalletPage} />
-          <Route path="/buy/" component={BuyCurrencyPage} />
+          <Route path="/wallet" component={WalletPage} />
+          {/* <Route path="/buy/" component={BuyCurrencyPage} />
           <Route path="/bank-statement/" component={BankStatementPage} /> */}
         </Switch>
       </BrowserRouter>
